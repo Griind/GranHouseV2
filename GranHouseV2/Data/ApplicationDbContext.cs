e2258457be416a9726a5ec71b12a4ebb72a7a19f
Roman Svitukha <roman.svitukha@slalom.com>
2018-12-12 14:59:32 -0600

    Initial checkin

--- GranHouseV2/Data/ApplicationDbContext.cs
+++ GranHouseV2/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GranHouseV2.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace GranHouseV2.Data
             : base(options)
         {
         }
+        public DbSet<ProductTypes> productTypes { get; set; }
     }
 }
